024bdf81773825e7abc0d4bd78f1d984dbbbed0e
alejandrasalazarcardona9@gmail.com <alejandrasalazarcardona9@gmail.com>
2025-03-26 21:14:14 -0500

    Actualizacion de edad

--- src/main/java/co/edu/umanizales/myfirstproject/controller/SellerController.java
+++ src/main/java/co/edu/umanizales/myfirstproject/controller/SellerController.java
@@ -15,7 +15,7 @@ public class SellerController {
 
         //Instanciar 5 sellers
 
-        Seller juan = new Seller("10304569", "Juan David", "Sanchez Perez", new Location( " 369", "Manizales"), (byte) 48, 'H');
+        Seller juan = new Seller("10304569", "Juan David", "Sanchez Perez", new Location( " 369", "Manizales"), (byte) 42, 'H');
         Seller andres = new Seller("10526787", "Andres felipe", "Lopez Velez", new Location("369", "Manizales"), (byte) 45, 'H');
         Seller sebastian = new Seller("1053808302", "Sebastian", "Murillo Morales", new Location("169", "Manizales"), (byte) 33, 'H');
         Seller manuela = new Seller("1053778156", "Manuela", "Salazar Diaz", new Location("635", "Manizales"), (byte) 36, 'M');
