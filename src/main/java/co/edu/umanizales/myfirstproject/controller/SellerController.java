464a96f2ed34456f2e3bcdbdf14a83a3a5629cc1
alejandrasalazarcardona9@gmail.com <alejandrasalazarcardona9@gmail.com>
2025-03-30 21:08:47 -0500

    Actualizacion de ciudades

--- src/main/java/co/edu/umanizales/myfirstproject/controller/SellerController.java
+++ src/main/java/co/edu/umanizales/myfirstproject/controller/SellerController.java
@@ -14,12 +14,17 @@ public class SellerController {
     public Seller getseller() {
 
         //Instanciar 5 sellers
+        Location medellin = new Location("001", "Medellin");
+        Location manizales = new Location("001","Manizales");
+        Location  tulua = new Location("834", "Tulua");
+        Location buenaventura = new Location("109","Buenaventura");
+        Location bogota= new Location("601", "Bogota");
 
-        Seller juan = new Seller("10304569", "Juan David", "Sanchez Perez", new Location( " 369", "Manizales"), (byte) 42, 'H');
-        Seller andres = new Seller("10526787", "Andres felipe", "Lopez Velez", new Location("369", "Manizales"), (byte) 45, 'H');
-        Seller sebastian = new Seller("1053808302", "Sebastian", "Murillo Morales", new Location("169", "Manizales"), (byte) 33, 'H');
-        Seller manuela = new Seller("1053778156", "Manuela", "Salazar Diaz", new Location("635", "Manizales"), (byte) 36, 'M');
-        Seller camila = new Seller("1094259761", "Maria camila", "Orozco Rincon", new Location("487", "Manizales"), (byte) 30, 'M');
+        Seller juan = new Seller("10304569", "Juan David", "Sanchez Perez", medellin, (byte) 42, 'H');
+        Seller andres = new Seller("10526787", "Andres felipe", "Lopez Velez", manizales, (byte) 45, 'H');
+        Seller sebastian = new Seller("1053808302", "Sebastian", "Murillo Morales", tulua, (byte) 33, 'H');
+        Seller manuela = new Seller("1053778156", "Manuela", "Salazar Diaz", buenaventura, (byte) 36, 'M');
+        Seller camila = new Seller("1094259761", "Maria camila", "Orozco Rincon", bogota, (byte) 30, 'M');
 
         return juan;
     }
