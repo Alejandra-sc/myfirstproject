4b833e9283e8da0aa9b3c31f39fd30bafe9dab62
Carlos Loaiza <carlos.loaiza@pragmaticingenieria.com>
2025-04-16 18:38:45 -0500

    Fix problems

--- src/main/java/co/edu/umanizales/myfirstproject/controller/LocationController.java
+++ src/main/java/co/edu/umanizales/myfirstproject/controller/LocationController.java
@@ -2,100 +2,28 @@ package co.edu.umanizales.myfirstproject.controller;
 
 import co.edu.umanizales.myfirstproject.model.Location;
 import co.edu.umanizales.myfirstproject.service.LocationService;
-import co.edu.umanizales.myfirstproject.service.locationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.Arrays;
 import java.util.List;
 
-/*
+
 @RestController
 @RequestMapping(path = "/location")
 public class LocationController {
 
-    @GetMapping
-    public Location getLocation() {
-        Location location = new Location("17001", "Manizales");
-
-
-        return location;
-
-    }
-}
-//Metodo GET para devolver una lista de location
-/*@GetMapping
-    public List<Location> getlocation() {
-    //Aqui estamos creando una lista de ejemplo. Podrias obtener estos datos de una base de datos.
-
-        return Arrays.asList(
-                new Location("001", "Location 1"),
-                new Location("002", "Location 2"),
-                new Location("003", "Location 3")
-
-        );
-
-    }
-    */
-
-  /* @Autowired
-    private locationService locationService;
+    @Autowired
+    private LocationService locationService;
 
     @GetMapping
-    public List<Location> getLocations() {
-        try {
-//Especifica la ruta del archivo CSV
-            String csvFilePath = "C:/Users/USUARIO/IdeaProjects/myfirstproject/locations.csv";
-            return locationService.readLocationsFromCSV(csvFilePath);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;//Manejo simple de la excepcion
-        }
+    public List<Location> getLocations(){
+
+        return locationService.getLocations();
+
     }
-*/
 
-@Autowired
-private LocationService locationService;
 
-      @GetMapping
-      public List<Location> getLocations(){
 
-          return locationService.getLocations();
-
-      }
-
-/*
-      @GetMapping(path = "/{code}")
-      public Location getLocationByCode(@PathVariable String code){
-          return locationService.getLocationByCode(code);
-
-      }
-
-      @GetMapping(path = "/states")
-      public List<Location> getLocationsByStates(){
-          return locationService.getStates();
-      }
-
-  /*
-@Autowired
-private locationService locationService;
-
-@GetMapping
-public List<Location> getLocations() {
-    try {
-        return locationService.readLocationsFromCSV();
-    } catch (IOException | URISyntaxException e) {
-        e.printStackTrace();
-        return null;//Manejo simple de la excepción
-    }
 }
-
-
-
-
-
---
+++ src/main/java/co/edu/umanizales/myfirstproject/service/LocationService.java
@@ -0,0 +1,51 @@
+package co.edu.umanizales.myfirstproject.service;
+
+import co.edu.umanizales.myfirstproject.model.Location;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+@Getter
+public class LocationService {
+
+    private List<Location> locations;
+
+    @Value("${locations_filename}")
+    private String locationsFilename;
+
+    @PostConstruct
+    public void readLocationsFromCSV() throws IOException, URISyntaxException {
+        locations = new ArrayList<>();
+
+
+        Path pathFile = Paths.get(ClassLoader.getSystemResource(locationsFilename).toURI());
+
+        try (CSVReader csvReader = new CSVReader(new FileReader(pathFile.toString()))) {
+            String[] line;
+            csvReader.skip(1);
+            // Ciclo para Leer todas las filas del CSV
+            while ((line = csvReader.readNext()) != null) {
+
+                // Crear un nuevo objeto Location y agregarlo a la lista
+                locations.add(new Location(line[2], line[3]));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;  // Lanza la excepción para que pueda manejarse en la capa superior si es necesario
+        } catch (CsvValidationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
